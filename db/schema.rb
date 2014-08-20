329d54158fe19750d0c5fca66f9fdd262e41863f
stiven sanchez <stivensanchez003@gmail.com>
2014-08-20 19:40:40 +0000

    creacion de la tabla areas y diseño de las mismas

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140820020836) do
+ActiveRecord::Schema.define(version: 20140820193430) do
+
+  create_table "areas", force: true do |t|
+    t.string   "name"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "documents", force: true do |t|
     t.string   "name"
