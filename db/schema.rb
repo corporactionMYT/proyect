2498243dd204776e00798018bbbd7ad796fa0eee
stiven sanchez <stivensanchez003@gmail.com>
2014-08-20 02:13:09 +0000

    creacion de la tabla user

--- db/schema.rb
+++ db/schema.rb
@@ -11,9 +11,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140820014928) do
+ActiveRecord::Schema.define(version: 20140820020836) do
 
   create_table "documents", force: true do |t|
+    t.string   "name"
+    t.string   "description"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "users", force: true do |t|
     t.string   "name"
     t.string   "description"
     t.datetime "created_at"
